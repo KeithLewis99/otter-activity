c79a69b168891f6e59c6109f6e1b230207a2a411
Keith Lewis <Keith.Lewis@dfo-mpo.gc.ca>
2024-09-24 13:15:09 -0230

    Major: added data import

--- otter_data.R
+++ otter_data.R
@@ -1,4 +1,6 @@
-# The purpose of this file is import and examine otter activity data from Terra Nova National Park (TNNP) and the Glovertown Newfoundland area.  Data provided by David Cote, 2024-09-23.
+# The purpose of this file is import and examine otter activity data from Terra Nova National Park (TNNP) and the Glovertown Newfoundland area.  Data provided by David Cote, 2024-09-23, from Chelsey Lawrence's thesis.
+
+# see notes from 2024-09-20 (notebook) for questions
 
 # Set up a project - see the below link for directions.
 #https://happygitwithr.com/rstudio-git-github.html
@@ -24,7 +26,8 @@ if(!dir.exists("refs"))dir.create("refs") #for rmd report
 
 # Start----
 #libraries
-library(readr)
+#library(readr)
+library(readxl)
 # library(dplyr)
 # library(tidyr)
 # library(ggplot2)
@@ -36,3 +39,20 @@ options(dplyr.print_max = 1e9)
 
 # Source files and objects
 #source("simpleLRP_FUN.R")
+
+
+# Data ----
+
+## camera activity data----
+## 
+df_act <- read_excel("data/camera_activity_5-11-2014.xlsx")
+str(df_act)
+
+
+## latrine distributions ----
+df_lat <- read_excel("data/latrine_distributions_29-7-2014.xlsx", sheet = "original")
+str(df_lat)
+
+## camera functioning ----
+df_cam <- read_excel("data/Camera_functioning.xlsx")
+str(df_cam)
